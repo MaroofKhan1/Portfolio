e66d8c65b64a86261d025ef36967cb4d7525d028
maroofkhan <khan.maroof6569@gmail.com>
2022-08-22 19:47:48 -0500

    updated skills

--- main.js
+++ main.js
@@ -3,27 +3,16 @@ const myTags = [
     'React','Python', 'Git',
     'Django', 'Node.js', 'Express',
     'Heroku', 'PostgreSQL', 'jQuery',
-    'Adobe Photoshop', 'Bootstrap',
+    'Adobe PhotoShop', 'Bootstrap',
     'MongoDB', 'Materialize', 'AWS' , 'MySql'
-    , 'VsCode'
+    ,'VsCode', 'C#','TailWind', 'Materialize'
 ];
 
 var tagCloud = TagCloud('.content', myTags,{
-
-  // radius in px
   radius: 250,
-
-  // animation speed
-  // slow, normal, fast
   maxSpeed: 'fast',
   initSpeed: 'fast',
-
-  // 0 = top
-  // 90 = left
-  // 135 = right-bottom
   direction: 135,
-  
-  // interact with cursor move on mouse out
   keep: true
   
 });
